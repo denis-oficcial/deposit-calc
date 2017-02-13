d119fe172a6aac87246575a44010c4358c6b9aae
Францев Денис Сергеевич <ri68s17@mwp02.csc.local>
2017-02-13 14:34:05 +0700

    Added entering and cheking of the data

--- main.c
+++ main.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+
+
+int main()
+{
+    int sum;
+    int time;
+    printf ("Please enter your deposit amount and term of the deposit:\n");
+    scanf ("%d %d", sum, time);
+    
+    if (sum >= 10 && time <= 365){
+        switch (){  
+        
+        }
+    }else{
+        printf("\nThe entered data is not valid.");
+    };
+    
+    return 0;
+}
