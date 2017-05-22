127201be05f5f40a75847884ff5358d42f04966d
Францев Денис Сергеевич <ri68s17@mwp02.csc.local>
2017-05-22 14:44:31 +0700

    Changed validation_test for 9lab

--- test/validation_test.c
+++ test/validation_test.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <ctest.h>
+#include <stdlib.h>
+#include <time.h>
 
 CTEST(validation_test, date_correct)
 {
 	int time;
-	scanf("%d", &time);
-
+	time = rand() % 366;
 	ASSERT_INTERVAL(0, 365, time);
 }
 
 CTEST(validation_test, sum_correct)
 {
 	int sum;
-	scanf("%d", &sum);
+	sum = random() % 1000 + 10;
 	ASSERT_TRUE(sum > 10);
 	ASSERT_INTERVAL(10,10000000, sum);
 	
